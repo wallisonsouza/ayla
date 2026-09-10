8c64acfc46acf807a132622345b1e5f10f14d30c
wallisonsouza <wallisonsouza200@gmail.com>
2026-09-10 09:52:34 -0300

    update build commands

--- build.sh
+++ build.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+
 set -e
 
 export CC=clang
 export CXX=clang++
 
 mkdir -p build
+
 cd build
 
 cmake -G Ninja \
--DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
--DCMAKE_C_FLAGS="-O0 -g" \
--DCMAKE_CXX_FLAGS="-O0 -g" \
--DCMAKE_C_COMPILER_LAUNCHER=ccache \
--DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
-..
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+    -DCMAKE_C_FLAGS="-O0 -g" \
+    -DCMAKE_CXX_FLAGS="-O0 -g" \
+    -DCMAKE_C_COMPILER_LAUNCHER=ccache \
+    -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
+    ..
 
 MAX_THREADS=2
 THREADS=$(nproc)
@@ -22,14 +24,8 @@ if [ "$THREADS" -gt "$MAX_THREADS" ]; then
     THREADS=$MAX_THREADS
 fi
 
-ninja -j $THREADS
-
-cd ..
-
-ln -sf build/compile_commands.json compile_commands.json
+ninja -j "$THREADS"
 
 clear
 
-cd build
-#gdb --args/
- ./ayla run ../src/ayla/scripts/main.ayla --dump ast
+./ayla run ../src/ayla/scripts/main.ayla --dump ast
